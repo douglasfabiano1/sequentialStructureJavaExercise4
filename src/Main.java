3d73e100298959531dfae538adedcfe20d21ee6a
Douglas Fabiano <douglasfabiano23@gmail.com>
2023-05-12 14:47:16 -0300

    Sequencial structure salary exercise

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Id do funcionário: ");
+        int id = sc.nextInt();
+        System.out.print("Número de horas trabalhadas: ");
+        int quantityHours = sc.nextInt();
+        System.out.print("Valor da hora trabalhada: ");
+        double valueHour = sc.nextDouble();
+
+        double salary = quantityHours * valueHour;
+
+        System.out.println("NUMBER = " + id);
+        System.out.printf("SALARY = U$ %.2f%n", salary);
+    }
+}
